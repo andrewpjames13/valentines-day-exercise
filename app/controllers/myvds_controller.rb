8c9aaa58cdaf4081cb14dea50cec6be0528fe993
Andrew James <andrewpjames87@gmail.com>
2015-02-13 09:05:10 -0700

    Lionel sings and youtube video for music

--- app/controllers/myvds_controller.rb
+++ app/controllers/myvds_controller.rb
@@ -1,6 +1,26 @@
 class MyvdsController < ApplicationController
 
   def index
+    @myvd = Myvd.all
   end
-  
+
+  def new
+    @myvd = Myvd.new
+  end
+
+  def create
+    @myvd = Myvd.new(myvd_params)
+    if @myvd.save
+      redirect_to myvds_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def myvd_params
+    params
+  end
+
 end
